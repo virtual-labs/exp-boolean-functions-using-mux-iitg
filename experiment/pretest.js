e4028ebc7f44cb575a354b900a4d5f0780c98331
IITGVLAB <75976828+IITGVLAB@users.noreply.github.com>
2021-01-18 12:37:44 +0530

    Update pretest.js

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -89,21 +89,25 @@
 
 
      const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1.It is possible for an enable or strobe input to undergo an expansion of two or more MUX ICs to the digital multiplexer with the proficiency of large number of:", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "Inputs", ///// Write the option 1 inside double quotes
+                b: "Outputs", ///// Write the option 2 inside double quotes
+                c: "Selection lines", ///// Write the option 2 inside double quotes
+                d: "Enable lines", ///// Write the option 2 inside double quotes
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
 
     {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
+      question: "A digital multiplexer is a combinational circuit that selects",  ///// Write the question inside double quotes
       answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
+        a: "One digital information from several sources and transmits the selected one",                  ///// Write the option 1 inside double quotes
+        b: "Many digital information and convert them into one",                  ///// Write the option 2 inside double quotes
+        c: "Many decimal inputs and transmits the selected information",  
+        d: "Many decimal outputs and accepts the selected information", 
+      },
+      correctAnswer: "a"                ///// Write the correct option inside double quotes
     },
 
 
